2ad9bc5f19db7c043a18999a8d6ba7a52963d031
任卓伦 <renzhuolun@lianqin360.com>
2017-03-29 13:10:12 +0800

    hibernate 类型数据扩展 cent、wanCent

---
+++ src/com/upg/ucars/framework/base/hibernate/CentMoneyType.java
@@ -0,0 +1,34 @@
+package com.upg.ucars.framework.base.hibernate;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.hibernate.HibernateException;
+import org.hibernate.type.BigDecimalType;
+
+@SuppressWarnings("serial")
+public class CentMoneyType extends BigDecimalType {
+
+	private static final BigDecimal	HUNDRED	= new BigDecimal(100);
+
+	@Override
+	public void set(PreparedStatement st, Object value, int index) throws HibernateException, SQLException {
+		if (value != null && value instanceof BigDecimal) {
+			value = ((BigDecimal) value).multiply(HUNDRED);
+		}
+		super.set(st, value, index);
+	}
+
+	@Override
+	public Object get(ResultSet rs, String name) throws HibernateException, SQLException {
+		Object result = super.get(rs, name);
+		if (result != null && result instanceof BigDecimal) {
+			result = new BigDecimal(BigDecimal.valueOf(Double.parseDouble(((BigDecimal)result).divide(HUNDRED, 2, BigDecimal.ROUND_HALF_UP).toString()))  
+		            .stripTrailingZeros().toPlainString());
+		}
+		return result;
+	}
+
+}
