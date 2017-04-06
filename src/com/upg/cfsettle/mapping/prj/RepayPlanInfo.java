344a854856c7aa196059969674cd7abb3a304f1d
任卓伦 <renzhuolun@lianqin360.com>
2017-04-06 19:31:34 +0800

    还款计划调整

---
+++ src/com/upg/cfsettle/mapping/prj/RepayPlanInfo.java
@@ -0,0 +1,40 @@
+package com.upg.cfsettle.mapping.prj;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class RepayPlanInfo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Byte ptype;
+	
+	private Date repayDate;
+
+	public Byte getPtype() {
+		return ptype;
+	}
+
+	public void setPtype(Byte ptype) {
+		this.ptype = ptype;
+	}
+
+	public Date getRepayDate() {
+		return repayDate;
+	}
+
+	public void setRepayDate(Date repayDate) {
+		this.repayDate = repayDate;
+	}
+
+	public RepayPlanInfo(Byte ptype, Date repayDate) {
+		this.ptype = ptype;
+		this.repayDate = repayDate;
+	}
+	
+	public RepayPlanInfo() {
+	}
+}
